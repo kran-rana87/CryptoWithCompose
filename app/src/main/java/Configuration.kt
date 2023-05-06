91e519957e223e3a3cc402895d7bff5de9955e5c
Karan Rana <Karan.Rana@unisuper.com.au>
2023-05-06 22:16:02 +1000

    Test checkin

--- app/src/main/java/Configuration.kt
+++ app/src/main/java/Configuration.kt
@@ -1,4 +1,8 @@
 object Constants {
     const val API_KEY = ""
     const val CRYPTO_BASE_URL = "https://min-api.cryptocompare.com"
+    const val DUMMY_VALUE = "https://min-api.cryptocompare.com"
+    const val DUMMY_VALUE2 = "https://min-api.cryptocompare.com"
+    const val DUMMY_VALUE3 = "https://min-api.cryptocompare.com"
+
 }
