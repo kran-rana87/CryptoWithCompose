8f044ff5ce2eabaebf635b974994e9e4f2a6f357
Karan Rana <Karan.Rana@unisuper.com.au>
2023-05-06 22:54:30 +1000

    Changes to Feature Branch 1
    Changes to Feature Branch 1

--- app/src/main/java/com/karan/coingecko/demo/ConfigurationMap.kt
+++ app/src/main/java/com/karan/coingecko/demo/ConfigurationMap.kt
@@ -1,6 +1,6 @@
 package com.karan.coingecko.demo
 
 object ConstantsMap {
-    const val API_KEY = ""
+    const val API_KEY = "CHanges on Feature Branch"
     const val CRYPTO_BASE_URL = "Changes on Master Branch"
 }
