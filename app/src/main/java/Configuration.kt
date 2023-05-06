4feaf310deae395ff20d227a584fb50e3811c285
Karan Rana <Karan.Rana@unisuper.com.au>
2023-05-06 22:26:42 +1000

    Update Configuration.kt
    Removing all unwanted values

--- app/src/main/java/Configuration.kt
+++ app/src/main/java/Configuration.kt
@@ -1,11 +1,4 @@
 object Constants {
     const val API_KEY = ""
     const val CRYPTO_BASE_URL = "https://min-api.cryptocompare.com"
-    const val DUMMY_VALUE = "https://min-api.cryptocompare.com"
-    const val DUMMY_VALUE2 = "https://min-api.cryptocompare.com"
-    const val DUMMY_VALUE3 = "https://min-api.cryptocompare.com"
-    const val DUMMY_VALUE4 = "https://min-api.cryptocompare.com"
-    const val DUMMY_VALUE5 = "https://min-api.cryptocompare.com"
-    const val CRYPTO_BASE_Master1 = "https://min-api.cryptocompare.com"
-
 }
