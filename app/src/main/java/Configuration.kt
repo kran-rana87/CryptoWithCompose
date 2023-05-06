b3ffb1fcc01220da2a3c71a0b45b0b64b7af66c1
Karan Rana <Karan.Rana@unisuper.com.au>
2023-05-06 22:27:44 +1000

    Update Configuration.kt
    Updated values for configuaration params

--- app/src/main/java/Configuration.kt
+++ app/src/main/java/Configuration.kt
@@ -1,11 +1,10 @@
 object Constants {
     const val API_KEY = ""
     const val CRYPTO_BASE_URL = "https://min-api.cryptocompare.com"
-    const val DUMMY_VALUE = "https://min-api.cryptocompare.com"
-    const val DUMMY_VALUE2 = "https://min-api.cryptocompare.com"
-    const val DUMMY_VALUE3 = "https://min-api.cryptocompare.com"
-    const val DUMMY_VALUE4 = "https://min-api.cryptocompare.com"
+    const val DUMMY_VALUE = ""
+    const val DUMMY_VALUE2 = "https://min-.cryptocompare.com"
+    const val DUMMY_VALUE3 = "https://-api.cryptocompare.com"
+    const val DUMMY_VALUE4 = "https://min-api..com"
     const val DUMMY_VALUE5 = "https://min-api.cryptocompare.com"
-    const val CRYPTO_BASE_Master1 = "https://min-api.cryptocompare.com"
-
+    const val CRYPTO_BASE_Master1 = "://min-api.cryptocompare.com"
 }
