13ef6317c321847d0fab002eb5ac30a0e638d047
Karan Rana <Karan.Rana@unisuper.com.au>
2023-05-06 22:55:20 +1000

    Changes to master branch
    Changes to master branch

--- app/src/main/java/com/karan/coingecko/demo/ConfigurationMap.kt
+++ app/src/main/java/com/karan/coingecko/demo/ConfigurationMap.kt
@@ -2,5 +2,5 @@ package com.karan.coingecko.demo
 
 object ConstantsMap {
     const val API_KEY = ""
-    const val CRYPTO_BASE_URL = "https://min-api.cryptocompare.com"
+    const val CRYPTO_BASE_URL = "Changes on Master Branch"
 }
