c043ee38f2fe5930fc49c45dc92601215c022f7f
Karan Rana <Karan.Rana@unisuper.com.au>
2023-05-25 10:18:39 +1000

    Updated Request Interceptor
    Updated Request Interceptor

--- common/src/main/java/com/karan/coingecko/demo/common/utils/Constants.kt
+++ common/src/main/java/com/karan/coingecko/demo/common/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.karan.coingecko.demo.common.utils
 
 object Constants {
     const val CRYPTO_BASE_URL = "https://min-api.cryptocompare.com"
-    const val API_KEY = "51f882da9f55039b4d45ae2d3e1cb06d36022de9bbbb3dd16a0d973d17e34e47"
+    const val API_KEY = "da56b87de51d0608f34ac6a81ee7dd9ddaface83bb42d358e08fe034158f70ae"
     const val BASE_URL_COIN_IMAGE = "https://www.cryptocompare.com/"
 
 }
--- data-network/src/main/java/com/karan/coingecko/demo/network/retrofit/RequestInterceptor.kt
+++ data-network/src/main/java/com/karan/coingecko/demo/network/retrofit/RequestInterceptor.kt
@@ -1,11 +1,13 @@
 package com.karan.coingecko.demo.network.retrofit
 
+import com.karan.coingecko.demo.common.utils.Constants
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class RequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
+        var request = chain.request()
+        request = request.newBuilder().addHeader("X-CoinAPI-Key", Constants.API_KEY).build()
         return chain.proceed(request)
     }
 }
