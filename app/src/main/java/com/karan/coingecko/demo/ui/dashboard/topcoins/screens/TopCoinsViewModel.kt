d7d9bcc124a9394199a1f3070b414499436e4736
Karan Rana <Karan.Rana@unisuper.com.au>
2023-06-02 00:30:32 +1000

    Sorting Updated
    Sorting logic for Top Coins

--- app/src/main/java/com/karan/coingecko/demo/ui/dashboard/topcoins/screens/TopCoinsViewModel.kt
+++ app/src/main/java/com/karan/coingecko/demo/ui/dashboard/topcoins/screens/TopCoinsViewModel.kt
@@ -1,6 +1,12 @@
 package com.karan.coingecko.demo.ui.dashboard.topcoins.screens
 
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
 import com.karan.coingecko.demo.common.utils.Response
 import com.karan.coingecko.demo.dispatchers.AppDispatchers
@@ -9,6 +15,7 @@ import com.karan.coingecko.demo.domain.models.TopCoinsData
 import com.karan.coingecko.demo.network.repository.TopCoinsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -17,35 +24,44 @@ class TopCoinsViewModel @Inject constructor(
     appDispatchers: AppDispatchers,
 ) :
     ViewModel() {
-    val coinData: StateFlow<TopCoinsUiState> =
-        coinData().map { data ->
-            when (data) {
-                is Response.Success ->
-                    TopCoinsUiState.Success(TopCoinsData(data.data!!.coinListDashboard))
-                is Response.Loading ->
-                    TopCoinsUiState.Loading
-                else ->
-                    TopCoinsUiState.Failure
+
+
+    private val _currentSortingModeFlow: MutableStateFlow<SortingMode> = MutableStateFlow(SortingMode.MARKET)
+    val currentSortingModeFlow = _currentSortingModeFlow.asStateFlow()
+
+
+    val coinData = coinData().combine(currentSortingModeFlow) { data, sortingMethod ->
+        when (data) {
+            is Response.Success -> {
+               val sortedList = when (sortingMethod) {
+                    SortingMode.MARKET -> data.data?.coinListDashboard?.sortedBy { coin -> coin.marketCap.toInt() }
+                    SortingMode.NAME -> data.data?.coinListDashboard?.sortedBy { coin -> coin.name }
+                }
+                TopCoinsUiState.Success(TopCoinsData(sortedList!!))
             }
+            is Response.Loading ->
+                TopCoinsUiState.Loading
+            else ->
+                TopCoinsUiState.Failure
         }
-            .flowOn(appDispatchers.io)
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.Lazily,
-                initialValue = TopCoinsUiState.Loading
-            )
+    }.flowOn(appDispatchers.io)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Lazily,
+            initialValue = TopCoinsUiState.Loading
+        )
+
 
     private fun coinData(): Flow<Response<TopCoinsData>> {
         return topCoinRepository.fetchTopCoins()
     }
 
     fun sortByName() {
-        //TODO
+        _currentSortingModeFlow.value = SortingMode.NAME
     }
 
-    //TODO
-    private val marketCapComparator = Comparator<Coin> { left, right ->
-        right.marketCap.toInt().compareTo(left.marketCap.toInt())
+    enum class SortingMode {
+        MARKET,
+        NAME
     }
-
 }
